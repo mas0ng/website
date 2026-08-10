24f8cfa5f18695ed6fbed88aacbdf749412cc01b
Mason <masongibbs82@gmail.com>
2026-08-10 19:06:01 +0100

    New icons for apps

--- public_assets/site/js/config/site-data.js
+++ public_assets/site/js/config/site-data.js
@@ -1,6 +1,6 @@
 window.MAS0NG_SITE = {
   loginUrl: 'https://auth.mas0ng.com/login?return_to=%2Fsecure%2Fapps%2F',
-  sharedNavUrl: 'https://sharedassets.mas0ng.com/navbar.js?v=20260723-mobile3',
+  sharedNavUrl: 'https://sharedassets.mas0ng.com/navbar.js?v=20260801-chat1',
   siteName: 'mas0ng.com',
   siteOrigin: '',
   nav: [
