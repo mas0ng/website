1fcf93c9127eeb15c29a9e79b5f10d1f9456288b
Mason <masongibbs82@gmail.com>
2026-09-06 21:49:08 +0100

    Keep legal contents sticky and refresh shared navbar palette

--- public_assets/site/js/config/site-data.js
+++ public_assets/site/js/config/site-data.js
@@ -1,6 +1,6 @@
 window.MAS0NG_SITE = {
   loginUrl: 'https://auth.mas0ng.com/login?return_to=%2Fsecure%2Fapps%2F',
-  sharedNavUrl: 'https://sharedassets.mas0ng.com/navbar.js?v=20260905-navbar15',
+  sharedNavUrl: 'https://sharedassets.mas0ng.com/navbar.js?v=20260906-navbar16',
   siteName: 'mas0ng.com',
   siteOrigin: '',
   nav: [
